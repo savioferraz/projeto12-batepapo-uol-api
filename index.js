7d0074753803c923e64342b7263a8dba88096e41
savioferraz <savio.ferraz88@gmail.com>
2022-09-01 18:33:40 -0300

    feat: add routes

--- index.js
+++ index.js
@@ -7,11 +7,52 @@ dotenv.config();
 const server = express();
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
-
-server.use(express.json());
-server.use(cors());
 mongoClient.connect().then(() => {
   db = mongoClient.db("batepapo_uol");
+
+  server.use(express.json());
+  server.use(cors());
+});
+
+server.post("/participants", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+server.get("/participants", async (req, res) => {
+  try {
+    const participants = await db.collection("participante").find().toArray();
+    res.send(participants);
+  } catch (error) {
+    res.sendStatus(400);
+  }
+});
+
+server.post("/messages", async (req, res) => {
+  const messages = req.body;
+  try {
+    await db.collection("mensagem");
+  } catch (error) {}
+});
+
+server.get("/messages", async (req, res) => {
+  const limit = req.params.limit;
+
+  try {
+    const messages = await db.collection("mensagem").find().toArray();
+    if (limit) {
+      res.send(messages.slice(-limit));
+    } else {
+      res.send(messages);
+    }
+  } catch (error) {
+    res.sendStatus(400);
+  }
+});
+
+server.post("/status", async (req, res) => {
+  try {
+  } catch (error) {}
 });
 
 server.listen(5000, () => console.log("Listening on port 5000"));
